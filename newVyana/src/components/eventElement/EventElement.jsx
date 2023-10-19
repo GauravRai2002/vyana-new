1e0dad613ee21a287340af507f6b02744ab59809
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2023-10-19 13:53:52 +0530

    Logo background removed

--- newVyana/src/components/eventElement/EventElement.jsx
+++ newVyana/src/components/eventElement/EventElement.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function EventElement({ logo, data }) {
   return (
     <Link to={`/event/${data.e_id}`}>
-      <div className='scale-90 duration-100 hover:scale-100 custom-background w-fit md:p-6 p-4 flex items-center justify-center  cursor-pointer'>
+      <div className='scale-90 duration-100 hover:scale-100  w-fit md:p-6 p-4 flex items-center justify-center  cursor-pointer'>
         <img className='md:w-32 w-12' src={logo} alt="" />
       </div>
     </Link>
