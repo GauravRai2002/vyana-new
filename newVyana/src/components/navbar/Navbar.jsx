cc51a7887c70ebcaf9b94dda0132f5fb5104dc05
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2023-11-01 17:24:15 +0530

    navigation issue resolved

--- newVyana/src/components/navbar/Navbar.jsx
+++ newVyana/src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
+  const navigate = useNavigate()
   return (
     <>
       <div className="navbar bg-gradient-to-r from-purple-500 to-pink-500">
@@ -11,12 +13,12 @@ function Navbar() {
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 text-white shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[2] p-2 text-white shadow bg-base-100 rounded-box w-52">
               {/* <li><Link to='/store' className='text-white'>
                 <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00024000000000000003"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.24000000000000005"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M5 3C3.89543 3 3 3.89543 3 5V6.83772L1.49006 11.3675C1.10052 12.5362 1.8474 13.7393 3 13.963V20C3 21.1046 3.89543 22 5 22H9H10H14H15H19C20.1046 22 21 21.1046 21 20V13.963C22.1526 13.7393 22.8995 12.5362 22.5099 11.3675L21 6.83772V5C21 3.89543 20.1046 3 19 3H5ZM15 20H19V14H17.5H12H6.5H5V20H9V17C9 15.3431 10.3431 14 12 14C13.6569 14 15 15.3431 15 17V20ZM11 20H13V17C13 16.4477 12.5523 16 12 16C11.4477 16 11 16.4477 11 17V20ZM3.38743 12L4.72076 8H6.31954L5.65287 12H4H3.38743ZM7.68046 12L8.34713 8H11V12H7.68046ZM13 12V8H15.6529L16.3195 12H13ZM18.3471 12L17.6805 8H19.2792L20.6126 12H20H18.3471ZM19 5V6H16.5H12H7.5H5V5H19Z" fill="#ffffff"></path> </g></svg>
                 STORE</Link></li> */}
               <li>
-                <Link to={'/about'} className='text-white'>About Us</Link>
+                  <Link to='/about' className='m-2 text-white'>About Us</Link>
               </li>
             </ul>
           </div>
