aeff3494fe56912704fbb5134b7de7f135028efa
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2023-12-04 13:43:11 +0530

    reverse removed

--- newVyana/src/components/invertedEvents/LatestEvents.jsx
+++ newVyana/src/components/invertedEvents/LatestEvents.jsx
@@ -10,22 +10,22 @@ function LatestEvents({ data }) {
   // data.reverse()
   const width = window.innerWidth
   const limit = width>1000?3:2
+
+
   console.log(data)
   useEffect(() => {
     AOS.init();
   }, [])
-  var i=0;
   return (
     <div id='events' data-aos="fade-up" data-aos-duration="2500" className='md:w-5/6 md:rounded-lg mx-auto border-solid border-2 border-[#0F1837] bg-blue-950 md:my-32 my-16 py-16 md:p-10 p-3 relative'>
         <div className='font-bold md:text-6xl text-xl text-center drop-shadow-lg text-white'>Our Events</div>
         <div className='flex flex-wrap overflow-y-hidden md:mt-16 my-2 items-center md:h-fit justify-center md:gap-24 mx-auto'>
-
           {
-            data.map((d,i)=>{
-              if (i<limit ) return <Link key={i} to={`/event/${d.e_id}`}><EventElement data={d} logo={d.e_logo}/></Link>
-              i++
-            })
+            data.length>0?<><Link key={0} to={`/event/${data[0].e_id}`}><EventElement data={data[0]} logo={data[0].e_logo}/></Link>
+            <Link key={1} to={`/event/${data[1].e_id}`}><EventElement data={data[1]} logo={data[1].e_logo}/></Link>
+            <Link key={2} to={`/event/${data[2].e_id}`}><EventElement data={data[2]} logo={data[2].e_logo}/></Link></>:<></>
           }
+        
         </div>
 
         <Link className='mx-auto mt-8 block w-40' to='/latestEvents'><button className=' btn btn-secondary btn-lg'>Show More</button></Link>
