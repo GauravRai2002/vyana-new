2632b77795404a06b285f4b63470ecc29117068a
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2024-01-04 11:17:41 +0530

    icon size adjusted

--- newVyana/src/components/eventElement/EventElement.jsx
+++ newVyana/src/components/eventElement/EventElement.jsx
@@ -6,7 +6,7 @@ function EventElement({ logo, data }) {
   return (
     <Link className='' to={`/event/${data.e_id}`}>
       <div className='flex flex-col items-center justify-center w-40'>
-        <div className='scale-90 duration-100 hover:scale-100 md:p-5 custom-background p-4 flex items-center justify-center  cursor-pointer'>
+        <div className='scale-90 duration-100 hover:scale-100 md:p-4 custom-background p-1 flex items-center justify-center  cursor-pointer'>
           <img className='md:w-32 w-22' src={logo} alt="" />
         </div>
         <div className='text-white font-md text-center font-bold' >{data.e_name}</div>
